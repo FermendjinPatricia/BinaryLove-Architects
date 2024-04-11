cd14c4fd93cc9d0dddbc5144b8275e4d166627e5
Duduman Andrei <aduduman@hamilton-ce.com>
2024-04-11 16:41:08 +0300

    Adjust login and register page

--- NearbyNights/app/(tabs)/_layout.tsx
+++ NearbyNights/app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Login Page',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -50,7 +50,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: 'Register page',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
